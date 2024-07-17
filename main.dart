08ef547bf14d9ae7ccf06ce7b9f5ed6257a20974
Mohamed Zakaria <116889612+mohamedzakaria9@users.noreply.github.com>
2024-07-18 02:04:59 +0300

    first task main file added

---
+++ main.dart
@@ -0,0 +1,72 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'dart:math';
+
+/*
+  Just please uncomment each point to test the code
+* */
+void main() {
+  //POINT_1
+  // String? string1 = stdin.readLineSync();
+  // String string2 = "";
+  // string2 = string1!.replaceAll(" ", "");
+  // print(string2);
+
+
+  //POINT_2
+  // double number = double.parse(stdin.readLineSync()!);
+  // int? persicion = int.parse(stdin.readLineSync()!);
+  // print(number.toStringAsPrecision(5));
+
+
+  //POINT_3
+  // String? string = stdin.readLineSync();
+  // stdout.write(string![0].toUpperCase());
+  // for(int i = 1 ; i < string.length ; i++){
+  //   stdout.write(string[i]);
+  // }
+
+
+  //POINT_4
+  // String? string1 = stdin.readLineSync();
+  // String? subString1 = stdin.readLineSync();
+  // if(string1!.contains(subString1!,0)){
+  //   print("The String ${string1} contains the substring ${subString1}");
+  // }else{
+  //   print("The is no relation between the two strings");
+  // }
+
+
+  //POINT_5
+  // int? setSize = int.parse(stdin.readLineSync()!);
+  // Set<int> numbers1 = Set();
+  // Set<int> numbers2 = Set();
+  // for(int i = 0 ; i < setSize ; i++){
+  //   int? number = int.parse(stdin.readLineSync()!);
+  //   numbers1.add(number);
+  // }
+  // for(int i = 0 ; i < setSize ; i++){
+  //   int? number = int.parse(stdin.readLineSync()!);
+  //   numbers2.add(number);
+  // }
+  // print(numbers1);
+  // print(numbers2);
+  // print("Intersection is ${numbers1.intersection(numbers2)}");
+  // print("Union is ${numbers1.union(numbers2)}");
+
+
+  //POINT_6
+  // int? listSize = int.parse(stdin.readLineSync()!);
+  // double total = 0;
+  // for(int i = 0 ; i < listSize ; i++){
+  //   double? number = double.parse(stdin.readLineSync()!);
+  //   total+=number;
+  // }
+  // print("Total = ${total}");
+  // print("Average : ${total/listSize}");
+
+  //POINT_&
+  // double? radius = double.parse(stdin.readLineSync()!);
+  // print("Premiter = ${2*pi*radius}");
+  // print("Area = ${pi*radius*radius}");
+}
