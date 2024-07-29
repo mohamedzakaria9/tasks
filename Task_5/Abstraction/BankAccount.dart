0ca7bf210e1ba0eacdd24124fb6744f2744f7725
Mohamed Zakaria <116889612+mohamedzakaria9@users.noreply.github.com>
2024-07-29 18:39:53 +0300

    Add files via upload

---
+++ Task_5/Abstraction/BankAccount.dart
@@ -0,0 +1,18 @@
+class BankAccount{
+  double _balance = 1000;
+  void deposit(double depositAmount){
+    if(depositAmount>0){
+      _balance+=depositAmount;
+    }else{
+      print("Sorry can't deposit this to you account");
+    }
+  }
+  void withdraw(double withdrawAmount){
+    if(withdrawAmount>0 && withdrawAmount>= _balance){
+      _balance-=withdrawAmount;
+    }else{
+      print("insufficient Credit");
+    }
+  }
+  double get balance => _balance;
+}
